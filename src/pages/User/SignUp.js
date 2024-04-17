527a8aa8b95975204bddf67602403513cfb37a66
SHLee334 <yogu970126@gmail.com>
2024-04-17 17:54:37 +0900

    <fix> : 회원가입 정규표현식 적용
    <fix> : 회원가입 정규표현식 적용
    - 회원가입 모든 input 항목에 정규표현식 조건 적용
    - 조건 미달에 따른 회원가입 제한은 아직

--- src/pages/User/SignUp.js
+++ src/pages/User/SignUp.js
@@ -26,11 +26,11 @@ const Div = styled.div`
     flex-direction: column;
   }
   .input-content input {
-    width: 400px;
+    width: 500px;
     padding: 15px 10px;
     margin-bottom: 5px;
   }
-  .input-content span {
+  .regExpMessage {
     height: 25px;
     color: coral;
   }
@@ -46,49 +46,176 @@ const Div = styled.div`
   }
 `;
 
-const idRegexp = /^[a-zA-Z][a-zA-Z0-9]{7,14}$/;
-const idText = "영문자 및 숫자 포함 7~15자 (단, 첫 글자는 영문자로)";
-
 const SignUp = () => {
+  // 정규표현식
+  const idRegexp = /^[a-zA-Z][a-zA-Z0-9]{7,14}$/; // 아이디
+  const pwdRegexp = /^[a-zA-Z0-9!-~]{8,14}$/; // 비밀번호
+  const nameRegexp = /^[a-zA-Z0-9가-힣]{2,20}$/; // 이름
+  const nickRegexp = /^[a-zA-Z0-9가-힣]{3,20}$/; // 닉네임
+  const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // 이메일
+  const phoneRegexp = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/; // 전화번호
+
+  // 정규표현식 미충족시 출력할 경고문구
+  const idText = "영어 대소문자 및 숫자 포함 7~15자 (단, 첫 글자는 영문자로)"; // 아이디
+  const pwdText = "영어 대소문자, 숫자 및 특수문자 포함 8~16자"; // 비밀번호
+  const pwdChkText = "동일한 비밀번호 입력 요망"; // 비밀번호체크
+  const nameText = "영어 대소문자 및 한글 2~20자"; // 이름
+  const nickText = "영어 대소문자 및 한글 3~20자"; // 닉네임
+  const emailText = "올바른 이메일 양식 입력 요망"; // 이메일
+  const phoneText = "하이픈(-) 포함해서 입력 요망"; // 전화번호
+
+  // 회원가입 후 메인화면 리다이렉트에 사용할 navigate
   const navigate = useNavigate();
-  const [user, setUser] = useState({});
 
-  const register = async () => {
-    await registerUser(user);
-    navigate("/");
-  };
+  const [user, setUser] = useState({ userId: "" });
 
-  const onChangeId = (e) => {
-    if (idRegexp.test(user.userId)) {
-      // IDSpan.innerHTML = "";
+  // 경고문구 초기화
+  const [userIdSpan, setUserIdSpan] = useState("");
+  const [userPwdSpan, setUserPwdSpan] = useState("");
+  const [userPwdCheckSpan, setUserPwdCheckSpan] = useState("");
+  const [userPersonNameSpan, setUserPersonNameSpan] = useState("");
+  const [userNicknameSpan, setUserNicknameSpan] = useState("");
+  const [userEmailSpan, setUserEmailSpan] = useState("");
+  const [userPhoneSpan, setUserPhoneSpan] = useState("");
+
+  /* --------------------------- 정규표현식 체크하는 함수 모음 --------------------------- */
+  // 입력한 아이디 정규표현식으로 체크하는 함수
+  const onChangeId = () => {
+    if (idRegexp.test(user.userId) || user.userId === "") {
+      setUserIdSpan("");
     } else {
-      // IDSpan.innerHTML = idText;
+      setUserIdSpan(idText);
     }
   };
 
+  // 입력한 비밀번호 정규표현식으로 체크하는 함수
+  const onChangePwd = () => {
+    if (pwdRegexp.test(user.userPwd) || user.userPwd === "") {
+      setUserPwdSpan("");
+    } else {
+      setUserPwdSpan(pwdText);
+    }
+  };
+
+  // 동일한 비멀번호 입력 체크하는 함수
+  const onChangeChkPwd = () => {
+    if (user.userPwd === user.userPwdCheck || user.userPwdCheck === "") {
+      setUserPwdCheckSpan("");
+    } else {
+      setUserPwdCheckSpan(pwdChkText);
+    }
+  };
+
+  // 입력한 이름 정규표현식으로 체크하는 함수
+  const onChangeName = () => {
+    if (nameRegexp.test(user.userPersonName) || user.userPersonName === "") {
+      setUserPersonNameSpan("");
+    } else {
+      setUserPersonNameSpan(nameText);
+    }
+  };
+
+  // 입력한 닉네임 정규표현식으로 체크하는 함수
+  const onChangeNick = () => {
+    if (nickRegexp.test(user.userNickname) || user.userNickname === "") {
+      setUserNicknameSpan("");
+    } else {
+      setUserNicknameSpan(nickText);
+    }
+  };
+
+  // 입력한 이메일 정규표현식으로 체크하는 함수
+  const onChangeEmail = () => {
+    if (
+      emailRegexp.test(user.userEmail) ||
+      user.userEmail === "" ||
+      user.userEmail === undefined
+    ) {
+      setUserEmailSpan("");
+    } else {
+      setUserEmailSpan(emailText);
+    }
+  };
+
+  // 입력한 전화번호 정규표현식으로 체크하는 함수
+  const onChangePhone = () => {
+    if (
+      phoneRegexp.test(user.userPhone) ||
+      user.userPhone === "" ||
+      user.userPhone === undefined
+    ) {
+      setUserPhoneSpan("");
+    } else {
+      setUserPhoneSpan(phoneText);
+    }
+  };
+
+  /* --------------------------- 정규표현식 실행하는 useEffect 모음 --------------------------- */
+  // 아이디 체크 정규표현식 실행하는 useEffect
   useEffect(() => {
     onChangeId();
-  }, [user]);
+  }, [user.userId]);
+
+  // 비밀번호 체크 정규표현식 실행하는 useEffect
+  useEffect(() => {
+    onChangePwd();
+  }, [user.userPwd]);
+
+  // 동일 비밀번호 체크 정규표현식 실행하는 useEffect
+  useEffect(() => {
+    onChangeChkPwd();
+  }, [user.userPwdCheck]);
+
+  // 이름 체크 정규표현식 실행하는 useEffect
+  useEffect(() => {
+    onChangeName();
+  }, [user.userPersonName]);
+
+  // 닉네임 체크 정규표현식 실행하는 useEffect
+  useEffect(() => {
+    onChangeNick();
+  }, [user.userNickname]);
+
+  // 이메일 체크 정규표현식 실행하는 useEffect
+  useEffect(() => {
+    onChangeEmail();
+  }, [user.userEmail]);
+
+  useEffect(() => {
+    onChangePhone();
+  }, [user.userPhone]);
+
+  /* --------------------------- 회원가입 작동 함수 --------------------------- */
+  // 조건을 만족하면 회원가입 버튼 작동
+  const register = async () => {
+    if (!idRegexp.test(user.userId)) {
+      alert("조건 불만족!");
+    } else {
+      await registerUser(user);
+      navigate("/");
+      alert("회원가입 완료! 로그인하세요");
+    }
+  };
 
   return (
     <Div>
       <div id="register">
         <div className="input-content">
-          <label for="userId">아이디</label>
+          <label htmlFor="userId">아이디</label>
           <div>
             <input
               type="text"
               id="userId"
-              value={user.userId || ""}
+              value={user.userId}
               onChange={(e) => {
                 setUser((prev) => ({ ...prev, userId: e.target.value }));
               }}
             />
-            <span id="userIdSpan"></span>
+            <span className="regExpMessage">{userIdSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userPwd">비밀번호</label>
+          <label htmlFor="userPwd">비밀번호</label>
           <div>
             <input
               type="password"
@@ -98,18 +225,26 @@ const SignUp = () => {
                 setUser((prev) => ({ ...prev, userPwd: e.target.value }));
               }}
             />
-            <span id="userPwd-span"></span>
+            <span className="regExpMessage">{userPwdSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userPwd-check">비밀번호 확인</label>
+          <label htmlFor="userPwd-check">비밀번호 확인</label>
           <div>
-            <input type="password" id="userPwd-check" />
-            <span id="userPwd-check-span"></span>
+            <input
+              type="password"
+              id="userPwdCheck"
+              name="userPwdCheck"
+              value={user.userPwdCheck || ""}
+              onChange={(e) => {
+                setUser((prev) => ({ ...prev, userPwdCheck: e.target.value }));
+              }}
+            />
+            <span className="regExpMessage">{userPwdCheckSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userPersonName">이름</label>
+          <label htmlFor="userPersonName">이름</label>
           <div>
             <input
               type="text"
@@ -122,11 +257,11 @@ const SignUp = () => {
                 }));
               }}
             />
-            <span id="userPersonName-span"></span>
+            <span className="regExpMessage">{userPersonNameSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userNickname">닉네임</label>
+          <label htmlFor="userNickname">닉네임</label>
           <div>
             <input
               type="text"
@@ -139,11 +274,11 @@ const SignUp = () => {
                 }));
               }}
             />
-            <span id="userNickname-span"></span>
+            <span className="regExpMessage">{userNicknameSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userEmail">이메일 주소</label>
+          <label htmlFor="userEmail">이메일 주소</label>
           <div>
             <input
               type="text"
@@ -156,11 +291,11 @@ const SignUp = () => {
                 }));
               }}
             />
-            <span id="userEmail-span"></span>
+            <span className="regExpMessage">{userEmailSpan}</span>
           </div>
         </div>
         <div className="input-content">
-          <label for="userPhone">전화번호</label>
+          <label htmlFor="userPhone">전화번호</label>
           <div>
             <input
               type="text"
@@ -173,7 +308,7 @@ const SignUp = () => {
                 }));
               }}
             />
-            <span id="userPhone-span"></span>
+            <span className="regExpMessage">{userPhoneSpan}</span>
           </div>
         </div>
         <button onClick={register}>회원가입</button>
