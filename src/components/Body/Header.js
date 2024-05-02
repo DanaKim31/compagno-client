cbb720f5976960d003665ecdee15bed231eec0ec
DanaKim31 <danakim731@gmail.com>
2024-05-02 12:08:44 +0900

    <feat> Header 각 페이지 링크 추가

--- src/components/Body/Header.js
+++ src/components/Body/Header.js
@@ -109,8 +109,8 @@ const Header = () => {
             </div>
             <div className="dropdown-content">
               <a href="compagno/animal-board">콘테스트</a>
-              <a href="#">제품정보 공유</a>
-              <a href="#">반려동물 동반</a>
+              <a href="/compagno/product-board">제품정보 공유</a>
+              <a href="content">반려동물 동반</a>
               <a href="/compagno/neighborBoard">우리동네 게시판</a>
             </div>
           </div>
@@ -120,14 +120,14 @@ const Header = () => {
             </div>
             <div className="dropdown-content">
               <a href="/compagno/sitterBoard">펫 시터</a>
-              <a href="#">원데이 클래스</a>
+              <a href="/compagno/onedayClassBoard">원데이 클래스</a>
             </div>
           </div>
           <div className="dropdown">
             <div className="dropbtn">공지사항</div>
             <div className="dropdown-content">
               <a href="#">공지사항</a>
-              <a href="#">QnA</a>
+              <a href="question">QnA</a>
             </div>
           </div>
           {Object.keys(user).length !== 0 ? (
@@ -170,7 +170,7 @@ const Header = () => {
                     <a href="#">콘테스트</a>
                   </li>
                   <li>
-                    <a href="#">제품정보 공유</a>
+                    <a href="/compagno/product-board">제품정보 공유</a>
                   </li>
                   <li>
                     <a href="#">반려동물 동반</a>
