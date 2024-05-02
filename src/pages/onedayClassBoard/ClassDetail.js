582e82ae1892aec3221659743e9aa6d7ab941b86
hnsso <152463185+hnsso@users.noreply.github.com>
2024-05-02 12:12:02 +0900

    hello

--- src/pages/onedayClassBoard/ClassDetail.js
+++ src/pages/onedayClassBoard/ClassDetail.js
@@ -1,3 +1,15 @@
-const ClassDetail = () => {};
+import {
+  onedayClass,
+  updateOnedayClass,
+  deleOnedayClass,
+} from "../../api/onedayClass";
+
+const ClassDetail = () => {
+  const viewClass = async () => {
+    const result = await onedayClass();
+  };
+
+  return <></>;
+};
 
 export default ClassDetail;
