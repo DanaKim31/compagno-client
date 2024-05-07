0a0155a2fb818c385325f4f1ce7733cef6bccc1e
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-05-07 12:42:04 +0900

    <feat>AdopBoardView

--- src/pages/adoptionBoard/AdopBoardView.js
+++ src/pages/adoptionBoard/AdopBoardView.js
@@ -1,2 +1,8 @@
-const viewAdopBoard = () => {};
-export default viewAdopBoard;
+const ViewAdopBoard = () => {
+  return (
+    <>
+      <h2>d안녀영녀ㅕㅇ녕너</h2>
+    </>
+  );
+};
+export default ViewAdopBoard;
