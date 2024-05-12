1cbf6784a30b3ab57b7677060f78e721e44d397e
SHLee334 <yogu970126@gmail.com>
2024-05-12 21:12:14 +0900

    <feat> : 마이페이지 > 입양 공고 페이지 추가
    <feat> : 마이페이지 > 입양 공고 페이지 추가

--- src/pages/user/MyPageAdoption.js
+++ src/pages/user/MyPageAdoption.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import MyPageSidebar from "../../components/user/MyPageSidebar";
 import MyPageTab from "../../components/user/MyPageTab";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import useDidMountEffect from "../../components/user/useDidMountEffect";
+import { getAdoptionList, getAdoptionCount } from "../../api/user";
+import Card from "react-bootstrap/Card";
 
 const Div = styled.div`
   @font-face {
@@ -29,18 +34,81 @@ const Div = styled.div`
       justify-content: center;
       align-items: center;
       flex-direction: column;
+
+      .myAdopList {
+        display: flex;
+        flex-direction: column;
+      }
     }
   }
 `;
 
 const MyPageAdoption = () => {
+  const [user, setUser] = useState({});
+  // 유저정보 가지고 오기
+  const info = useSelector((state) => {
+    return state.user;
+  });
+
+  useEffect(() => {
+    if (Object.keys(info).length === 0) {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    } else {
+      setUser(info);
+    }
+  }, []);
+
+  useDidMountEffect(() => {
+    adopListAPI();
+  }, [user]);
+
+  const [adopList, setAdopList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [adopCount, setAdopCount] = useState(0);
+
+  // 입양 리스트 + 갯수 가져오기
+  const adopListAPI = async () => {
+    const response = await getAdoptionList(user.userId, page);
+    const adopData = response.data;
+    setAdopList(adopData);
+
+    const countResponse = await getAdoptionCount(user.userId);
+    const countAdopData = countResponse.data;
+    setAdopCount(countAdopData);
+
+    console.log(adopData);
+  };
+
+  // 페이지 변경
+  const handlePageChange = async (page) => {
+    setPage(page);
+    const response = await getAdoptionList(user.userId, page);
+    const adopData = response.data;
+    setAdopList(adopData);
+  };
+
   return (
     <Div>
       <MyPageSidebar />
       <div className="myAdopMain">
         <MyPageTab />
         <div className="contentZone">
-          <p>입양공고 정보</p>
+          <Card style={{ width: "18rem" }}>
+            {adopList?.map((adopInfo) => (
+              <div className="myAdopList">
+                <Card.Img variant="top" src={adopInfo.adopAnimalImage} />
+                <Card.Body>
+                  <Card.Title>{adopInfo.adopAnimalKind}</Card.Title>
+                  <Card.Text>
+                    <p>{adopInfo.adopAnimalGender}</p>
+                    <p>{adopInfo.adopAnimalNeuter}</p>
+                    <p>{adopInfo.adopAnimalFindplace}</p>
+                    <p>{adopInfo.adopRegiDate}</p>
+                  </Card.Text>
+                </Card.Body>
+              </div>
+            ))}
+          </Card>
         </div>
       </div>
     </Div>
