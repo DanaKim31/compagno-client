d44d472343cec6cf7a416528ecbbcf1964a69e83
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-05-17 16:15:06 +0900

    <fix> 메인 화면 UI 변경

--- src/components/Body/Section6.js
+++ src/components/Body/Section6.js
@@ -1,30 +1,51 @@
 import "../../assets/style.css";
+import React, { useState, useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { userSave } from "../../store/user";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Section6 = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => {
+    return state.user;
+  });
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token !== null) {
+      dispatch(userSave(JSON.parse(localStorage.getItem("user"))));
+    }
+  }, []);
+
+  const navigate = useNavigate();
+  const sendNote = () => {
+    if (Object.keys(user).length === 0) {
+      navigate("/compagno/login");
+    } else {
+      navigate("/compagno/mypage/mynote");
+    }
+  };
   return (
     <>
       <section id="section6">
         <div className="message">
           <div className="content">
-            <h1>Messenger</h1>
+            <h1>Note</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto nam nisi natus voluptates ea magnam ex. Incidunt
-              doloribus quibusdam excepturi? Rerum autem amet iusto quidem
-              eveniet labore fugiat dignissimos veniam! Eos culpa corrupti
-              eligendi cupiditate, incidunt quia illum asperiores ex distinctio
-              debitis placeat unde cum, sapiente assumenda eaque provident natus
-              impedit, hic in! Quae eveniet eaque asperiores eos necessitatibus!
-              Consequuntur. Debitis aspernatur beatae enim velit suscipit quod
-              totam dicta sequi earum repellendus nisi, aliquam recusandae
-              ratione at soluta possimus sunt eos odio, quasi libero perferendis
-              ipsum! Ex perferendis iste provident. Aperiam ipsam, corrupti
-              inventore quisquam a nemo eveniet itaque vero, veniam libero error
-              rem nostrum sequi ipsum dignissimos tempora cum architecto
-              voluptatibus aliquam suscipit natus consequatur vel recusandae
-              quis? Ab.
+              The note function on the homepage makes it easy to communicate
+              between users. <br />
+              Sending a note is simple, and you can write a message directly
+              from the other person's profile.
+              <br /> The notebox can manage both sent and received notes.
+              <br /> Users can also attach files and send and receive various
+              types of documents through the note function.
+              <br /> Important notes can be managed separately.
+              <br /> Any note that you want to delete will ultimately be deleted
+              if both users delete it.
             </p>
-            <a href="">유저들과의 대화를 원하시나요</a>
+            {/* <a href="">유저들과의 쪽지를 원하시나요</a> */}
+            <button onClick={sendNote}>유저들과의 쪽지를 원하시나요</button>
           </div>
           <div className="photo">
             <img src="/img/section6-(1).jpg" alt="logo image" />
