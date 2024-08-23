1432484b7e7e73d083b466392d41f3c19df3fb27
김현수 <152463185+hnsso@users.noreply.github.com>
2024-08-23 23:39:05 +0900

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,3 +11,17 @@ const Layout = () => {
   );
 };
 export default Layout;
+
+// import { Outlet } from "react-router-dom";
+// import Header from "./Body/Header";
+// import "../assets/reset.css";
+
+// const Layout = () => {
+//   return (
+//     <>
+//       <Header />
+//       <Outlet />
+//     </>
+//   );
+// };
+// export default Layout;
